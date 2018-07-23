17c25f594a81daba1f093d7adaa55229393d068e
Belvain <heikki.kurhinen@metatavu.fi>
2018-07-23 10:26:05 +0300

    Fixed esc arming sequency

--- blueController_receiver/blueController_receiver.ino
+++ blueController_receiver/blueController_receiver.ino
@@ -5,89 +5,78 @@
 
 #define CE_PIN 9
 #define CSN_PIN 10
-#define RADIO_LED_PIN 6
+#define RADIO_LED_PIN LED_BUILTIN
 #define ESC_SIGNAL_PIN 3
 #define ESC_VCC_PIN 5
 #define ESC_LOW_PULSE 1000
+#define ESC_MID_PULSE 1500
 #define ESC_HIGH_PULSE 2000
-#define DISCONNECT_DELAY 1000
+#define ARM_PULL_HIGH_DELAY 900
+#define ARM_HIGH_PULSE_WIDTH 100
+#define DISCONNECT_DELAY 500
 
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 RF24 radio(CE_PIN, CSN_PIN);
 
+int currentSpeed = ESC_MID_PULSE;
 int joystick[2];
 Servo esc;
-boolean armed = false;
-boolean escConnected = false;
 boolean connectionLost = false;
+boolean armed = false;
+boolean arming = false;
 unsigned long connectionLostTime;
+unsigned long armStartTime;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(ESC_VCC_PIN, OUTPUT);
   pinMode(RADIO_LED_PIN, OUTPUT);
-  digitalWrite(ESC_VCC_PIN, LOW);
   digitalWrite(RADIO_LED_PIN, LOW);
+  digitalWrite(ESC_VCC_PIN, HIGH);
   esc.attach(ESC_SIGNAL_PIN);
-  delay(1000);
   radio.begin();
   radio.openReadingPipe(1,pipe);
   radio.startListening();
 }
 
 void loop() {
-  if(radio.available()) {
-    digitalWrite(RADIO_LED_PIN, HIGH);
-    connectionLost = false;
-    bool done = false;
-    while (!done) {
-      done = radio.read( joystick, sizeof(joystick) );
-      if(!escConnected) {
-        connectEsc();
+  if (!armed) {
+    if (!arming) {
+      arming = true;
+      armStartTime = millis();
+    } else {
+      unsigned long currentTime = millis();
+      if(currentTime - armStartTime > ARM_PULL_HIGH_DELAY) {
+        currentSpeed = ESC_HIGH_PULSE;
+        digitalWrite(RADIO_LED_PIN, HIGH);
       }
-      if(!armed) {
-        arm();
-      } else {
-        int newSpeed =  map(joystick[0], 512, 1023, ESC_LOW_PULSE, ESC_HIGH_PULSE);
-        esc.writeMicroseconds(newSpeed);
-        Serial.println(newSpeed);
-        //Serial.println("Setting pulse: "+newSpeed);
+      
+      if (currentTime - armStartTime > (ARM_PULL_HIGH_DELAY + ARM_HIGH_PULSE_WIDTH)) {
+        currentSpeed = ESC_MID_PULSE;
+        digitalWrite(RADIO_LED_PIN, LOW);
+        armed = true;
       }
     }
   } else {
-    if(escConnected && !connectionLost) {
-      connectionLost = true;
-      connectionLostTime = millis();
-    }
-    if(escConnected && connectionLost) {
-      unsigned long currentTime = millis();
-      if(currentTime - connectionLostTime > DISCONNECT_DELAY) {
-        Serial.println("Connection to transmitter lost");
-        digitalWrite(RADIO_LED_PIN, LOW);
-        if(armed) {
-          esc.writeMicroseconds(ESC_LOW_PULSE);
+    if(radio.available()) {
+      digitalWrite(RADIO_LED_PIN, HIGH);
+      connectionLost = false;
+      bool done = false;
+      while (!done) {
+        done = radio.read( joystick, sizeof(joystick) );
+        currentSpeed = map(joystick[0], 0, 1023, ESC_LOW_PULSE, ESC_HIGH_PULSE);
+      }
+    } else {
+      if(!connectionLost) {
+        connectionLost = true;
+        connectionLostTime = millis();
+      } else {
+        if(millis() - connectionLostTime > DISCONNECT_DELAY) {
+          currentSpeed = ESC_MID_PULSE;
         }
-        disconnectEsc();
       }
     }
   }
-}
-
-void arm() {
-  Serial.println("Arming ESC");
-  esc.writeMicroseconds(ESC_LOW_PULSE);
-  delay(1000);
-  armed = true;
-}
-
-void connectEsc() {
-  digitalWrite(ESC_VCC_PIN, HIGH);
-  escConnected = true;
-  delay(1000);
-}
-
-void disconnectEsc() {
-  digitalWrite(ESC_VCC_PIN, LOW);
-  escConnected = false;
+  esc.writeMicroseconds(currentSpeed);
+  delay(5);
 }
